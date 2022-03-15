e6a1ca0ace1f25b32a979ee8e04088484d18ca2a
kukwa clovis <kukwaclovisngong3@gmail.com>
2022-03-15 17:31:22 +0100

    installing vue to use for the frontend UI and creating the client folder. setting up frontend and backend running.

---
+++ client/crypto-site/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>welcome to advanced crypto and forex site.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  h1 {
+    font: 800 100px sans-serif;
+    padding: 20px;
+    text-transform: capitalize;
+  }
+}
+</style>
--- server/middlewares/user_sign_up.js
+++ server/middlewares/user_sign_up.js
@@ -13,8 +13,9 @@ router.route('/signup').get((req, res) => {
      res.send('this is the user signup route')
 }).post((req, res) => {
      console.log(req.body)
+     console.log(req.body.username)
      console.log('this is the user route')
-     res.send('helloo');
+     res.json(req.body);
      res.end()
 })
 
